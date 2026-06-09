b798844747a640d073212ce011ef2d141267a792
Time And Time Studio <nok.craft.studio@gmail.com>
2026-06-09 11:12:49 +0700

    Update player attributes in Data_Player class

--- Core/Player/Data.cs
+++ Core/Player/Data.cs
@@ -1,7 +1,7 @@
 public static class Data_Player
 {
     public static int[] hp = new int[] {10,3,2};
-    public static int[] attack = new int[] {2,1,1};
+    public static int[] attack = new int[] {1,1,1};
     public static int[] speed = new int[] {250,350,300};
     public static int[] cooldown_attack = new int[] {2,1,3};
 
@@ -10,7 +10,7 @@ public static class Data_Player
 
     public static string[] player_name_file = new string[] {"player-1","player-2","player-3"};
     public static int[] player_cooldown = new int[] {5,5,7};
-    public static int[] player_coin = new int[] {50,35,40};
+    public static int[] player_coin = new int[] {70,35,40};
     
     public static float[] player_hit_box = new float[] {3,3,3};
     public static float[] player_hit_box_size = new float[] {0,0,1.6f};
